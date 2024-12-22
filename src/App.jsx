89a1deeb4c4397f0c688d3717c3421749ad929d2
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-22 19:06:45 +0200

    Finished project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl font-semibold text-orange-500 text-center my-10">
+        Welcome to awwards
+      </h1>
+    </main>
+  )
+}
+
+export default App
