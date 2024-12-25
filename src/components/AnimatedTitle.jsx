6fdc2026530dac975f7ccd27410d4332574bbc97
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-25 21:07:23 +0200

    Added about section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.9] md:text-[6rem]">
+      Discover the w<b>o</b>rld's <br /> l<b>a</b>rgest shared adventure
+    </div>
+  )
+}
+
+export default AnimatedTitle
