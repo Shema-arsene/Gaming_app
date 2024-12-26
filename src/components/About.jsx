b69236184736cd858f7c8268fa889ed9e53738e3
Shema-arsene <aniyonkurushema@gmail.com>
2024-12-26 09:12:03 +0200

    Implemented the animated title component. It'll be used throughout the app

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
         </h2>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared <b>a</b>dventure"
+          title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure"
           containerClass="mt-5 !text-black text-center"
         />
 
--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,9 +1,50 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+// import
 
-const AnimatedTitle = () => {
+const AnimatedTitle = ({ title, containerClass }) => {
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    // useGSAP
+    const context = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      })
+
+      titleAnimation.to(".animated-word", {
+        opacity: 1,
+        transform: "translate3D(0,0,0) rotateY(0deg) rotateX(0deg)",
+        ease: "power2.inOut",
+        stagger: 0.2,
+      })
+    }, containerRef)
+
+    return () => context.revert()
+  }, [])
+
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.9] md:text-[6rem]">
-      Discover the w<b>o</b>rld's <br /> l<b>a</b>rgest shared adventure
+    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+      {title.split("<br/>").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, i) => (
+            <span
+              key={i}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
